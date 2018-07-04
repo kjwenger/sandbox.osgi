a3ddc2508a3fdf944b0857f9bffd47a2200f478b
Klaus Wenger <kjwenger@yahoo.com>
2018-07-04 13:52:01 +0200

    initial load

--- cpp/script/setup.chroot.sh
+++ cpp/script/setup.chroot.sh
@@ -5,15 +5,3 @@ SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR="$(dirname "${SCRIPTS_DIR}")"
 STAGING_DIR="${PROJECT_DIR}/staging"
 BUILD_DIR="${PROJECT_DIR}/build"
-
-cd "${PROJECT_DIR}"
-mkdir -p "${BUILD_DIR}"
-pushd "${BUILD_DIR}"
-rm -Rf *
-cmake \
-      -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-      -DCMAKE_INSTALL_PREFIX="${STAGING_DIR}" \
-      ..
-make -j
-popd
-cd "${CURRENT_DIR}"
--- jvm/src/main/java/com/example/sandbox/osgi/impl/Activator.java
+++ jvm/src/main/java/com/example/sandbox/osgi/impl/Activator.java
@@ -1,4 +1,15 @@
 package com.example.sandbox.osgi.impl;
 
-public class Activator {
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        System.out.println("Bundle 'com.example.sandbox.osgi' is starting");
+    }
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        System.out.println("Bundle 'com.example.sandbox.osgi' is stopping");
+    }
 }
